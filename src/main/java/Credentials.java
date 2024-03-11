988fd06fd2be3f590b4980aae91ee3efb60f55f0
marstonad20 <marstonad20@gcc.edu>
2024-03-11 15:27:14 -0400

    Added constructor with barebones Javadoc comment.

--- src/main/java/Credentials.java
+++ src/main/java/Credentials.java
@@ -5,16 +5,33 @@ public class Credentials {
     private String major;
     private String password;
     private String email;
+
+    /**
+     * Constructor
+     * @param newName
+     * @param newId
+     * @param newMajor
+     * @param newPassword
+     * @param newEmail
+     */
     public Credentials(
             String newName,
             int newId,
             String newMajor,
             String newPassword,
             String newEmail
-    ){}
-    public void login(String attemptEmail, String attemptPassword){}
-    public Boolean checkValid(String password){
-        return false;
+    ){
+        name = newName;
+        id = newId;
+        major = newMajor;
+        password = newPassword;
+        email = newEmail;
+    }
+    public void login(String attemptEmail, String attemptPassword){
+    }
+
+    public Boolean checkValid(String password) {
+        return password.equals(this.password);
     }
 
 }
