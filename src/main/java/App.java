accd482115b2d1843dcd27af8e754a84cd404618
byardHK <ByardHK21@GCC.edu>
2024-03-13 18:58:12 -0400

    Added implementation to allow for switching pages and finished login screen ui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,6 @@ public class Frame extends JFrame {
 
     Frame(){
         PageManager pm = new PageManager();
-        pm.switchPages("login-page");
-        LoginPage loginPage = new LoginPage(pm);
-        this.add(loginPage);
         this.setTitle("Scheduling App");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
