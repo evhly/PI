a6b1c368ab301a31e579d652fad7dff4baacc7fa
marstonad20 <marstonad20@gcc.edu>
2024-03-26 16:51:30 -0400

    Untested and incomplete! Added filtering to search() method to include whether a course in the database has the exact professor or department specified. Also provisionally implemented addProfessor() and addDepartment(). Also added equals to Professor class.

--- src/main/java/Professor.java
+++ src/main/java/Professor.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Professor {
     private String firstName;
     private String lastName;
@@ -12,4 +14,16 @@ public class Professor {
         return lastName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Professor professor = (Professor) o;
+        return Objects.equals(firstName, professor.firstName) && Objects.equals(lastName, professor.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
 }
