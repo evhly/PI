481c14592b0449e0e2cb48dc30556d1e87962da2
Allison Harnly <amharnly@msn.com>
2024-04-03 19:45:08 -0400

    borders and font sizes

--- src/main/java/CustomTableCellRenderer.java
+++ src/main/java/CustomTableCellRenderer.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 
@@ -10,13 +11,18 @@ public class CustomTableCellRenderer extends DefaultTableCellRenderer {
     {
         JComponent cell = (JComponent) super.getTableCellRendererComponent
                 (table, value, isSelected, hasFocus, row, column);
-//        cell.setBorder(new MatteBorder( 0, 0, 0, 1, Color.black));
+        cell.setFont(new Font("Arial", Font.PLAIN, 10));
+
         if(!value.toString().isEmpty()){
 //            System.out.println("here I am: " + row + ", " + column + ", " + value + ".");
             cell.setBackground(new Color(194,111,109));
 
         } else {
             cell.setBackground(Color.white);
+        }
+
+        if(row % 4 == 0){
+            cell.setBorder(new MatteBorder( 1, 0, 0, 0, Color.black));
         }
         return cell;
     }
