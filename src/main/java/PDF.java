58ab27bae9a420446b055157eb12c94b928a68d2
MARSTONAD20 <marstonad20@gcc.edu>
2024-04-28 15:39:20 -0400

    I made a fricking PDF LET'S GOOOOOOOOOOOOOOOO!!!

--- src/main/java/PDF.java
+++ src/main/java/PDF.java
@@ -1,12 +1,34 @@
-import javax.swing.text.Document;
+
 
 //TODO: implement
 
-public class PDF {
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
-    public Document create(Schedule schedule){
-        return null;
+import java.io.File;
+import java.io.IOException;
+
+
+public class PDF  {
+    public static void create (Schedule schedule) throws IOException {
+        PDDocument doc = new PDDocument();
+        PDPage page = new PDPage();
+        doc.addPage(page);
+
+        PDPageContentStream contentStream = new PDPageContentStream(doc,page);
+        contentStream.beginText();
+        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 10);
+        contentStream.newLineAtOffset(8,200);
+        contentStream.showText("Hello world");
+        contentStream.endText();
+        contentStream.close();
+
+        doc.save(new File("C:\\Users\\MARSTONAD20\\IdeaProjects\\PI\\src\\main\\PDFs\\simple.pdf"));
+        doc.close();
     }
-    public void export(){}
+
+    // public void export(){}
 
 }
---
+++ src/test/java/PDFTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class PDFTest {
+
+    @Test
+    void pdfHelloWorld() throws IOException {
+        PDF.create(null);
+    }
+}
